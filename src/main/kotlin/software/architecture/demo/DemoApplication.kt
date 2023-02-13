7b5d12c6f5efb39f4a858f52ec099135fe1ffa43
charlie2913 <carlos.ernesto.camargo.leon@gmail.com>
2023-02-13 01:34:33 -0400

    Creacion del repositorio

---
+++ src/main/kotlin/software/architecture/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package software.architecture.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
